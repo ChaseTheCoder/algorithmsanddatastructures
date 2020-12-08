03933048fb27c59887fe12b86df5f73228546f2e
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-07 22:01:49 -0600

    Big O Notations

---
+++ bigONotation.js
@@ -0,0 +1,11 @@
+// OBJECTIVES
+// Motivate the need for something like Big O notation
+// Describewhat Big O Notation is
+// Simplify Big O Exrpessions
+// Define "time complexity" and "space complexity"
+// Evaluate the time complexity and space complexity of different algorithms using  Big O notation
+// Describe what a logarithm is
+
+// BIG O NOTATION
+// It allows us to talk formally about how the runtime of an algorithm grows as the inputs grow.  
+// It's worried about worst case scenario
