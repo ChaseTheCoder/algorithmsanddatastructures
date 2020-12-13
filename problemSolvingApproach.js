8d49c9dc27a5f4e50e7e35e7d28c16a19088a89a
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-13 11:06:24 -0600

    new file problemSolvingApproach, problem solving steps

---
+++ problemSolvingApproach.js
@@ -0,0 +1,10 @@
+// ALGORITHM
+//  PROCESS OR SET OF STEPS TO ACCOMPLISH A CERTAIN TASK
+
+// PROBLEM SOLVING STEPS
+// 1 Understand the problem
+// 2 Explore concrete examples
+// 3 Break it down
+// 4 Solve / simplify
+// 5 Look back and refactor
+
