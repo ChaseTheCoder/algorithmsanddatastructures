58cadd9ae57d4eaba5e03fcda991f4798ae77927
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-18 21:28:11 -0600

    modified multiplePointersPatterns.js to inlcude Coding Exercise

--- solvingPatterns/frequencyCounter.js
+++ solvingPatterns/frequencyCounter.js
@@ -25,8 +25,8 @@ function same(arr1, arr2) {
   }
   let frequencyCounter1 = {}
   let frequencyCounter2 = {}
-  for(let val of arr1){
-    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  for(let val of arr1){ //way to iterate through
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1  // || 0, will initiate a value and then +1 will add that initial value
   }
   for(let val of arr2){
     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
@@ -46,3 +46,55 @@ function same(arr1, arr2) {
 
 same([1,2,3,2], [9,1,4,4])
 same([1,1,1,2], [2,1,2,2])
+
+
+// ANAGRAM -----------------------------------------------
+// MY SOLUTION =========================================
+// function validAnagram(ana1, ana2){
+//   if(ana1.length !== ana2.length){
+//       return false
+//   }
+//   let charSplit1 = ana1.split('')
+//   let charSplit2 = ana2.split('')
+//   let frequencyCounter1 = {}
+//   let frequencyCounter2 = {}
+//   for(let val of charSplit1){
+//       frequencyCounter1[val] = (frequencyCounter1[val] || 0) +1
+//   }
+//   for(let val of charSplit2){
+//       frequencyCounter2[val] = (frequencyCounter2[val] || 0) +1
+//   }
+//   for(let key in frequencyCounter1){
+//       if(!(key in frequencyCounter2)){
+//           return false
+//       }
+//       if(frequencyCounter2[key] !== frequencyCounter1[key]){
+//           return false
+//       }
+//   }
+//   return true
+// }
+
+// validAnagram("aaz", "zza")
+// validAnagram("anagram", "nagaram")
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letters exists, increment, otherwise set to 1 
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+
+  return true
+}
--- solvingPatterns/multiplePointersPatterns.js
+++ solvingPatterns/multiplePointersPatterns.js
@@ -1,12 +1,39 @@
 // Multiple Pointers Patterns
 
 //  function called sumZero which accepts a sorted array of integers. Function should find the first pair where the sum is 0.  Returned an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+//==============================================================================
+// BAD  O(n^2)
+// function sumZero(arr){
+//   for(let i = 0; i < arr.length; i++){
+//     for(let j = i+1; j < arr.length; j++){
+//       if(arr[i] + arr[j] === 0){
+//         return [arr[i], arr[j]];
+//       }
+//     }
+//   }
+// }
+
+// GOOD 0(n)
 function sumZero(arr){
-  for(let i = 0; i < arr.length; i++){
-    for(let j = i+1; j < arr.length; j++){
-      if(arr[i] + arr[j] === 0){
-        return [arr[i], arr[j]];
-      }
+  let left = 0;
+  let right = arr.length -1;
+  while(left < right){
+    let sum = arr[left] + arr[right];
+    if(sum === 0){                      //is it zeor?
+      return [arr[lef], arr[right]];
+    } else if(sum > 0){                 // i.e. if right abs value bigger go down index
+      right--;
+    } else {                          // else left abs value bigger so go up index
+      left++;
     }
   }
 }
+
+sumZero([-4,-3,-2,-1,0,5,10])
+
+//==============================================================================
+//countUniqueValues
+// implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. ther can be negative numbers in the array, ubt it will always be sorted.
+
+// GOOD
