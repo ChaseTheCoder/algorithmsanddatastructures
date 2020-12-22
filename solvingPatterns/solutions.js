efad84c52befa4681d21b0714c4cd927a579f240
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-22 11:20:19 -0600

    solutions for first two coding exercises added to solvingPatterns/solutions

--- solvingPatterns/solutions.js
+++ solvingPatterns/solutions.js
@@ -32,7 +32,59 @@ function sameFrequency(num1, num2){
 }
 
 // STEELE
+function sameFrequency1(num1,  num2){
+  let strNum1 = num1.tostring();
+  let strNum2 = num2.tostring();
 
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for(let i = 0; i < strNum1.length; i++){
+    countNum1[strNum1[i]] = (countNum1[strNum1[j]] || 0) + 1
+  }
+
+  for(let j = 0; j < strNum2.length; j++){
+    countNum2[strNum1[j]] = (countNum2[strNum2[j]] || 0) + 1
+  }
+
+  for(let key in countNum1){
+    if(countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
 
 // FREQUENCY COUNTER/ MULTIPLE POINTERS: areThereDuplicates
 // accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed. You can solve this using the frequency counter pattern OR the multiple pointers pattern
+// areThereDuplicates Solution (Frequency Counter)
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true
+  }
+  return false;
+}
+
+// areThereDuplicates Solution (Multiple Pointers)
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+
+//areThereDuplicates One Liner Solution
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
