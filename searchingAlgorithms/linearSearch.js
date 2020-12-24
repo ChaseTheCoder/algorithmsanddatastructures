9272345ceab94fe28fe5596f64b4636ee87aa07c
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-24 16:24:50 -0600

    searchingAlgorithms directory created

--- searchingAlgorithms/linearSearch.js
+++ searchingAlgorithms/linearSearch.js
@@ -1,1 +1,7 @@
- 
+// LINEAR SEARCH
+// Given an array, the simplest way to search for a value is to look at every element in the array and check if it's the value we want.
+// JavaScript methods:
+// indexOf
+// includes
+// find
+// findIdex 
