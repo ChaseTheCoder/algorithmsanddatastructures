3eee523ea16e24e714ef41f64c8675010cbd34a0
ChaseTheCoder <chasefsheaff@gmail.com>
2020-12-29 13:25:00 -0600

    modified naiveStringSearch

--- searchingAlgorithms/naiveStringSearch.js
+++ searchingAlgorithms/naiveStringSearch.js
@@ -1,1 +1,35 @@
 // NAIVE STRING SEARCH
+// find if there is a match in order
+function naiveSearch(long, short){
+  let count = 0;
+  for(let i = 0; i < long.length; i++){ //loop  through long
+    for(let j = 0; j < short.length; j++){  // loop through short
+      console.log(short[j], long[i+j])
+      if(short[j] !== long[i+j]){ // if index of short doesn't match index of long
+        console.log("Break!")
+        break; // break the loop
+      }
+      if(j === short.length - 1){  //if j === length because loop didn't break
+        console.log("found one")
+        count++; // add to count
+      }
+    }
+  }
+  console.log(count)
+  return count;  // once loop is done return count
+}
+
+naiveSearch("lorie loled", "lol"); // 1
+
+// Same, but shortened
+function naiveSearch(long, short){
+  let count = 0;
+  for(let i = 0; i < long.length; i++){ //loop  through long
+    for(let j = 0; j < short.length; j++){  // loop through short
+      console.log(short[j], long[i+j])
+      if(short[j] !== long[i+j]) break;
+      if(j === short.length - 1) count++;
+    }
+  }
+  return count;  // once loop is done return count
+}
