2e0eaa9a2940a57bf8ceb3ac0b46d2be8ad539f6
ChaseTheCoder <chasefsheaff@gmail.com>
2021-02-01 10:27:03 -0600

    small changes to linearSearch.js

--- searchingAlgorithms/linearSearch.js
+++ searchingAlgorithms/linearSearch.js
@@ -18,30 +18,30 @@
 // find middle, then left and right
 
 // BINARY SEARCH PSEUDOCODE
-// accepts sorted array and value
-// create a left pointer at the start of the array, and a right pointer at the end of the array
-// while left pointer comes before the right pointer:
-//  -create a pointer in the middle
-//  -if you find the value you want, return index
-//  -if the value is too small, move the left pointer up
+
+
+
+
+
+
 //  -if the value is too large, move the right pointer down
 //  if you never find the value, return -1
 // GOOD
-function binarySearch(arr, elem){
-  let start = 0;
-  let end = arr.length -1;
-  let middle = Math.floor((start + end) / 2);
+function binarySearch(arr, elem){ // accepts sorted array and value
+  let start = 0; // create a left pointer at the start of the array
+  let end = arr.length -1; // and a right pointer at the end of the array
+  let middle = Math.floor((start + end) / 2); //  -create a pointer in the middle
   //console.log(start, middle, end)
-  while(arr[middle] !== elem && start <= end){
+  while(arr[middle] !== elem && start <= end){ // while left pointer comes before the right pointer:
       //console.log(start, middle, end)
-    if(elem < arr[middle]){
+    if(elem < arr[middle]){  //  -if the value is too small, move the left pointer up
       end = middle -1;
     } else {
       start = middle + 1;
     } 
     let middle = Math.floor((start + end) / 2);
   }
-  if(arr[middle] === elem){
+  if(arr[middle] === elem){  //  -if you find the value you want, return index
     return middle;
   }
   return -1;
