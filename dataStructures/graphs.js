a89633d75b41160d3f587789a81b4ad8bc851b99
ChaseTheCoder <chasefsheaff@gmail.com>
2021-02-05 08:45:11 -0600

    graphs.js started

---
+++ dataStructures/graphs.js
@@ -0,0 +1,28 @@
+// GRAPHS
+// A graph data structure consists of a finite (and possibly mutalbe)
+// set of vetices or nodes or points,
+// together with a set of unordered pairs of these vertices for an 
+// undirected graph or a set of ordered pairs for a directed graph
+// i.e. NODES & CONNECTIONS 
+
+// USES FOR GRAPHS (a bunch of nodes connected to eachother)
+// Social Networks, location/mapping, routing algorithms
+// visual hierarchy, file system optimizations, EVERYWHERE!
+// recommendations "people also watched", "people you might know"
+
+// GRAPH TERMS
+// VERTEX - a node
+// EDGE - connection between nodes
+// WEIGHTED/UNWEIGHTED - values assigned to distances between vertices
+// DIRECTED/UNDIRECTED - directions assigned to distanced between vertices
+
+// Directed Graph
+// a direction assigned to an edge such as:
+// two friends follow eachother, arrows pointing at both vetices
+// one preson follows a famous person, arrow pointing only at famous person
+// WEIGHTED GRAPH
+// shows the value between vertices on edges
+// i.e. distances of roads, how much someone gives attention to someone they follow
+// UNDIRECTED GRAPH
+// just shows connections that are connected, looks like web
+// no values on edge
